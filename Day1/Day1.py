9bb6238990ca29a23fae7dd87a75431f47cdebc5
Kunkulis <kunkulis@inbox.lv>
2024-07-26 19:14:16 +0300

    day1 done

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,6 +1,23 @@
-def main():    
-    input_data = open('\\input.txt', 'r', encoding='utf-8').read().strip()
-    for i in range(len(input_data)):
-        print(input_data[i])
+input_data = open('advent01.txt').read().strip()
 
-main()
+def calculate_floor(input_data):
+    floor = 0
+    for letter in input_data:
+        if letter == '(':
+            floor += 1
+        else:
+            floor -= 1
+    return floor
+
+def find_basement(input_data):
+    floor = 0
+    for position, letter in enumerate(input_data, 1):
+        if letter == '(':
+            floor += 1
+        else:
+            floor -= 1
+        if floor == -1:
+            return position
+        
+print(calculate_floor(input_data))
+print(find_basement(input_data))
